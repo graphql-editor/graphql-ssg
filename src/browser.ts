ff470923f1a27b766786fabe6f3a17fe1c5e9a33
aexol <aexol@aexol.com>
2021-06-01 14:06:46 +0200

    disabled sandbox

--- src/browser.ts
+++ src/browser.ts
@@ -1,7 +1,10 @@
 import puppeteer from 'puppeteer';
 
 export const runBrowser = async (port: number) => {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
   const page = await browser.newPage();
   page.setCacheEnabled(false);
   await page.goto(`http://127.0.0.1:${port}`);
